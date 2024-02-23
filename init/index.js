00b81c1e99faeeed79557b7bf312871ad67a1cfd
Akshayswagh <waghakshay186@gmail.com>
2024-02-23 23:19:23 +0530

    phase 2 completed

--- init/index.js
+++ init/index.js
@@ -1,23 +1,27 @@
-const mongoose = require('mongoose');
-const {data} = require("./data.js");
+const mongoose = require("mongoose");
+const  initData  = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-    .then(() => {
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-    async function main() {
-        await mongoose.connect(MONGO_URL);
-    }
-    const initDB  = async () => {
-      await Listing.deleteMany({});
-      await Listing.insertMany(data);
-      console.log("data was intialized");
-    };
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "65d25e718c8f0d6f1fd3ebf4",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was intialized");
+};
 
-    initDB();
+initDB();
--- routes/listing.js
+++ routes/listing.js
@@ -1,96 +1,100 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapasync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
-
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-    if (error) {
-      let errMsg = error.details.map((el) => el.message).join(",");
-      throw new ExpressError(400, errMsg);
-    } else {
-      next();
-    }
-  };
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 // index route
 router.get(
-    "/",
-    wrapAsync(async (req, res) => {
-      const allListings = await Listing.find({});
-      res.render("listings/index.ejs", { allListings });
-    })
-  );
-  
-  // New Route
-  router.get("/new", (req, res) => {
-    res.render("listings/new.ejs");
-  });
-  
-  // show route
-  router.get(
-    "/:id",
-    wrapAsync(async (req, res) => {
-      let { id } = req.params;
-      const listing = await Listing.findById(id).populate("reviews");
-      if(!listing){
-        req.flash("error", "Listing  you requested for does not exist");
-        res.redirect("/listings");
-      }
-      res.render("listings/show.ejs", { listing });
-    })
-  );
-  
-  // Create Route
-  router.post(
-    "/",
-    validateListing,
-    wrapAsync(async (req, res, next) => {
-      const newListing = new Listing(req.body.listing);
-      await newListing.save();
-      req.flash("success", "New Listing Created!");
-      res.redirect("/listings");
-    })
-  );
-  
-  // Edit Route
-  router.get(
-    "/:id/edit",
-    wrapAsync(async (req, res) => {
-      let { id } = req.params;
-      const listing = await Listing.findById(id);
-      if(!listing){
-        req.flash("error", "Listing  you requested for does not exist");
-        res.redirect("/listings");
-      }
-      res.render("listings/edit.ejs", { listing });
-    })
-  );
-  
-  // update route
-  router.put(
-    "/:id",
-    validateListing,
-    wrapAsync(async (req, res) => {
-      let { id } = req.params;
-      await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-      req.flash("success", "Listing Updated!");
-      res.redirect(`/listings/${id}`);
-    })
-  );
-  
-  // Delete route
-  router.delete(
-    "/:id",
-    wrapAsync(async (req, res) => {
-      let { id } = req.params;
-      let deleteListing = await Listing.findByIdAndDelete(id);
-      req.flash("success", "Listing Deleted!");
-      res.redirect("/listings");
-    })
-  );
-  
+  "/",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
 
-  module.exports = router;
+// New Route
+router.get("/new", isLoggedIn, (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// show route
+router.get(
+  "/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id)
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "author",
+        },
+      })
+      .populate("owner");
+    if (!listing) {
+      req.flash("error", "Listing  you requested for does not exist");
+      res.redirect("/listings");
+    }
+    console.log(listing);
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+// Create Route
+router.post(
+  "/",
+  isLoggedIn,
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    await newListing.save();
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings");
+  })
+);
+
+// Edit Route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+      req.flash("error", "Listing  you requested for does not exist");
+      return res.redirect("/listings");
+    }
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+// update route
+router.put(
+  "/:id",
+  isLoggedIn,
+  isOwner,
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+// Delete route
+router.delete(
+  "/:id",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing Deleted!");
+    res.redirect("/listings");
+  })
+);
+
+module.exports = router;
