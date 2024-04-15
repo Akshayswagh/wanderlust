33516ddd1f2c207c1637d031b3fcfc3eef9ca3d3
Akshayswagh <waghakshay186@gmail.com>
2024-04-15 16:17:35 +0530

    all styling completed footer commented

--- public/js/script.js
+++ public/js/script.js
@@ -19,4 +19,34 @@
       false
     );
   });
-})();
+});
+
+// tax-toggle option
+
+// Index-Page-Card //
+let taxSwitch = document.getElementById("flexSwitchCheckDefault");
+taxSwitch.addEventListener("click", () => {
+  let taxInfo = document.getElementsByClassName("tax-info");
+  for (info of taxInfo) {
+    if (info.style.display != "inline") {
+      info.style.display = "inline";
+    } else {
+      info.style.display = "none";
+    }
+  }
+});
+
+// nav bar
+
+// Navbar //
+const navBarToggleBtn = document.getElementById("navbar-profile-btn");
+
+function clicked() {
+  let popup = document.getElementById("navbar-popup");
+  if (popup.style.display !== "block") {
+    popup.style.display = "block";
+  } else {
+    popup.style.display = "none";
+  }
+}
+navBarToggleBtn.addEventListener("click", clicked);
